e5f2cf16217eb3cb07caf1536aab299ad0412a95
RassiLevine <rassilevine@gmail.com>
2024-07-15 19:52:30 -0400

    #3

--- EmojiMemoryGameApp/EmojiMemoryGameApp/EmojiGame.Designer.cs
+++ EmojiMemoryGameApp/EmojiMemoryGameApp/EmojiGame.Designer.cs
@@ -28,18 +28,478 @@
         /// </summary>
         private void InitializeComponent()
         {
+            tblMain = new TableLayoutPanel();
+            tblButton = new TableLayoutPanel();
+            btn20 = new Button();
+            btn19 = new Button();
+            btn18 = new Button();
+            btn17 = new Button();
+            btn16 = new Button();
+            btn15 = new Button();
+            btn14 = new Button();
+            btn13 = new Button();
+            btn12 = new Button();
+            btn11 = new Button();
+            btn10 = new Button();
+            btn9 = new Button();
+            btn8 = new Button();
+            btn7 = new Button();
+            btn6 = new Button();
+            btn5 = new Button();
+            btn4 = new Button();
+            btn3 = new Button();
+            btn2 = new Button();
+            btn1 = new Button();
+            btnStart = new Button();
+            lblPlayer1 = new Label();
+            lblPlayerTwo = new Label();
+            tbl1 = new TableLayoutPanel();
+            lblScore1 = new Label();
+            txtScore1 = new TextBox();
+            tableLayoutPanel1 = new TableLayoutPanel();
+            txtScore2 = new TextBox();
+            lblScore2 = new Label();
+            txtMsg = new TextBox();
+            tblMain.SuspendLayout();
+            tblButton.SuspendLayout();
+            tbl1.SuspendLayout();
+            tableLayoutPanel1.SuspendLayout();
             SuspendLayout();
+            // 
+            // tblMain
+            // 
+            tblMain.ColumnCount = 3;
+            tblMain.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 22.2222214F));
+            tblMain.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 55.5555573F));
+            tblMain.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 22.2222214F));
+            tblMain.Controls.Add(tblButton, 1, 1);
+            tblMain.Controls.Add(btnStart, 0, 2);
+            tblMain.Controls.Add(lblPlayer1, 0, 0);
+            tblMain.Controls.Add(lblPlayerTwo, 2, 0);
+            tblMain.Controls.Add(tbl1, 0, 1);
+            tblMain.Controls.Add(tableLayoutPanel1, 2, 1);
+            tblMain.Controls.Add(txtMsg, 1, 0);
+            tblMain.Dock = DockStyle.Fill;
+            tblMain.Location = new Point(0, 0);
+            tblMain.Name = "tblMain";
+            tblMain.RowCount = 3;
+            tblMain.RowStyles.Add(new RowStyle(SizeType.Percent, 11.23348F));
+            tblMain.RowStyles.Add(new RowStyle(SizeType.Percent, 70.70485F));
+            tblMain.RowStyles.Add(new RowStyle(SizeType.Percent, 17.9487171F));
+            tblMain.Size = new Size(800, 450);
+            tblMain.TabIndex = 0;
+            // 
+            // tblButton
+            // 
+            tblButton.ColumnCount = 5;
+            tblButton.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 20F));
+            tblButton.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 20F));
+            tblButton.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 20F));
+            tblButton.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 20F));
+            tblButton.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 20F));
+            tblButton.Controls.Add(btn20, 4, 3);
+            tblButton.Controls.Add(btn19, 3, 3);
+            tblButton.Controls.Add(btn18, 2, 3);
+            tblButton.Controls.Add(btn17, 1, 3);
+            tblButton.Controls.Add(btn16, 0, 3);
+            tblButton.Controls.Add(btn15, 4, 2);
+            tblButton.Controls.Add(btn14, 3, 2);
+            tblButton.Controls.Add(btn13, 2, 2);
+            tblButton.Controls.Add(btn12, 1, 2);
+            tblButton.Controls.Add(btn11, 0, 2);
+            tblButton.Controls.Add(btn10, 4, 1);
+            tblButton.Controls.Add(btn9, 3, 1);
+            tblButton.Controls.Add(btn8, 2, 1);
+            tblButton.Controls.Add(btn7, 1, 1);
+            tblButton.Controls.Add(btn6, 0, 1);
+            tblButton.Controls.Add(btn5, 4, 0);
+            tblButton.Controls.Add(btn4, 3, 0);
+            tblButton.Controls.Add(btn3, 2, 0);
+            tblButton.Controls.Add(btn2, 1, 0);
+            tblButton.Controls.Add(btn1, 0, 0);
+            tblButton.Dock = DockStyle.Fill;
+            tblButton.Location = new Point(180, 53);
+            tblButton.Name = "tblButton";
+            tblButton.RowCount = 4;
+            tblButton.RowStyles.Add(new RowStyle(SizeType.Percent, 25F));
+            tblButton.RowStyles.Add(new RowStyle(SizeType.Percent, 25F));
+            tblButton.RowStyles.Add(new RowStyle(SizeType.Percent, 25F));
+            tblButton.RowStyles.Add(new RowStyle(SizeType.Percent, 25F));
+            tblButton.Size = new Size(438, 312);
+            tblButton.TabIndex = 0;
+            // 
+            // btn20
+            // 
+            btn20.BackColor = Color.Yellow;
+            btn20.Dock = DockStyle.Fill;
+            btn20.Location = new Point(351, 237);
+            btn20.Name = "btn20";
+            btn20.Size = new Size(84, 72);
+            btn20.TabIndex = 19;
+            btn20.UseVisualStyleBackColor = false;
+            // 
+            // btn19
+            // 
+            btn19.BackColor = Color.Yellow;
+            btn19.Dock = DockStyle.Fill;
+            btn19.Location = new Point(264, 237);
+            btn19.Name = "btn19";
+            btn19.Size = new Size(81, 72);
+            btn19.TabIndex = 18;
+            btn19.UseVisualStyleBackColor = false;
+            // 
+            // btn18
+            // 
+            btn18.BackColor = Color.Yellow;
+            btn18.Dock = DockStyle.Fill;
+            btn18.Location = new Point(177, 237);
+            btn18.Name = "btn18";
+            btn18.Size = new Size(81, 72);
+            btn18.TabIndex = 17;
+            btn18.UseVisualStyleBackColor = false;
+            // 
+            // btn17
+            // 
+            btn17.BackColor = Color.Yellow;
+            btn17.Dock = DockStyle.Fill;
+            btn17.Location = new Point(90, 237);
+            btn17.Name = "btn17";
+            btn17.Size = new Size(81, 72);
+            btn17.TabIndex = 16;
+            btn17.UseVisualStyleBackColor = false;
+            // 
+            // btn16
+            // 
+            btn16.BackColor = Color.Yellow;
+            btn16.Dock = DockStyle.Fill;
+            btn16.Location = new Point(3, 237);
+            btn16.Name = "btn16";
+            btn16.Size = new Size(81, 72);
+            btn16.TabIndex = 15;
+            btn16.UseVisualStyleBackColor = false;
+            // 
+            // btn15
+            // 
+            btn15.BackColor = Color.Yellow;
+            btn15.Dock = DockStyle.Fill;
+            btn15.Location = new Point(351, 159);
+            btn15.Name = "btn15";
+            btn15.Size = new Size(84, 72);
+            btn15.TabIndex = 14;
+            btn15.UseVisualStyleBackColor = false;
+            // 
+            // btn14
+            // 
+            btn14.BackColor = Color.Yellow;
+            btn14.Dock = DockStyle.Fill;
+            btn14.Location = new Point(264, 159);
+            btn14.Name = "btn14";
+            btn14.Size = new Size(81, 72);
+            btn14.TabIndex = 13;
+            btn14.UseVisualStyleBackColor = false;
+            // 
+            // btn13
+            // 
+            btn13.BackColor = Color.Yellow;
+            btn13.Dock = DockStyle.Fill;
+            btn13.Location = new Point(177, 159);
+            btn13.Name = "btn13";
+            btn13.Size = new Size(81, 72);
+            btn13.TabIndex = 12;
+            btn13.UseVisualStyleBackColor = false;
+            // 
+            // btn12
+            // 
+            btn12.BackColor = Color.Yellow;
+            btn12.Dock = DockStyle.Fill;
+            btn12.Location = new Point(90, 159);
+            btn12.Name = "btn12";
+            btn12.Size = new Size(81, 72);
+            btn12.TabIndex = 11;
+            btn12.UseVisualStyleBackColor = false;
+            // 
+            // btn11
+            // 
+            btn11.BackColor = Color.Yellow;
+            btn11.Dock = DockStyle.Fill;
+            btn11.Location = new Point(3, 159);
+            btn11.Name = "btn11";
+            btn11.Size = new Size(81, 72);
+            btn11.TabIndex = 10;
+            btn11.UseVisualStyleBackColor = false;
+            // 
+            // btn10
+            // 
+            btn10.BackColor = Color.Yellow;
+            btn10.Dock = DockStyle.Fill;
+            btn10.Location = new Point(351, 81);
+            btn10.Name = "btn10";
+            btn10.Size = new Size(84, 72);
+            btn10.TabIndex = 9;
+            btn10.UseVisualStyleBackColor = false;
+            // 
+            // btn9
+            // 
+            btn9.BackColor = Color.Yellow;
+            btn9.Dock = DockStyle.Fill;
+            btn9.Location = new Point(264, 81);
+            btn9.Name = "btn9";
+            btn9.Size = new Size(81, 72);
+            btn9.TabIndex = 8;
+            btn9.UseVisualStyleBackColor = false;
+            // 
+            // btn8
+            // 
+            btn8.BackColor = Color.Yellow;
+            btn8.Dock = DockStyle.Fill;
+            btn8.Location = new Point(177, 81);
+            btn8.Name = "btn8";
+            btn8.Size = new Size(81, 72);
+            btn8.TabIndex = 7;
+            btn8.UseVisualStyleBackColor = false;
+            // 
+            // btn7
+            // 
+            btn7.BackColor = Color.Yellow;
+            btn7.Dock = DockStyle.Fill;
+            btn7.Location = new Point(90, 81);
+            btn7.Name = "btn7";
+            btn7.Size = new Size(81, 72);
+            btn7.TabIndex = 6;
+            btn7.UseVisualStyleBackColor = false;
+            // 
+            // btn6
+            // 
+            btn6.BackColor = Color.Yellow;
+            btn6.Dock = DockStyle.Fill;
+            btn6.Location = new Point(3, 81);
+            btn6.Name = "btn6";
+            btn6.Size = new Size(81, 72);
+            btn6.TabIndex = 5;
+            btn6.UseVisualStyleBackColor = false;
+            // 
+            // btn5
+            // 
+            btn5.BackColor = Color.Yellow;
+            btn5.Dock = DockStyle.Fill;
+            btn5.Location = new Point(351, 3);
+            btn5.Name = "btn5";
+            btn5.Size = new Size(84, 72);
+            btn5.TabIndex = 4;
+            btn5.UseVisualStyleBackColor = false;
+            // 
+            // btn4
+            // 
+            btn4.BackColor = Color.Yellow;
+            btn4.Dock = DockStyle.Fill;
+            btn4.Location = new Point(264, 3);
+            btn4.Name = "btn4";
+            btn4.Size = new Size(81, 72);
+            btn4.TabIndex = 3;
+            btn4.UseVisualStyleBackColor = false;
+            // 
+            // btn3
+            // 
+            btn3.BackColor = Color.Yellow;
+            btn3.Dock = DockStyle.Fill;
+            btn3.Location = new Point(177, 3);
+            btn3.Name = "btn3";
+            btn3.Size = new Size(81, 72);
+            btn3.TabIndex = 2;
+            btn3.UseVisualStyleBackColor = false;
+            // 
+            // btn2
+            // 
+            btn2.BackColor = Color.Yellow;
+            btn2.Dock = DockStyle.Fill;
+            btn2.Location = new Point(90, 3);
+            btn2.Name = "btn2";
+            btn2.Size = new Size(81, 72);
+            btn2.TabIndex = 1;
+            btn2.UseVisualStyleBackColor = false;
+            // 
+            // btn1
+            // 
+            btn1.BackColor = Color.Yellow;
+            btn1.Dock = DockStyle.Fill;
+            btn1.Location = new Point(3, 3);
+            btn1.Name = "btn1";
+            btn1.Size = new Size(81, 72);
+            btn1.TabIndex = 0;
+            btn1.UseVisualStyleBackColor = false;
+            // 
+            // btnStart
+            // 
+            btnStart.BackColor = Color.Red;
+            btnStart.Dock = DockStyle.Fill;
+            btnStart.Location = new Point(3, 371);
+            btnStart.Name = "btnStart";
+            btnStart.Size = new Size(171, 76);
+            btnStart.TabIndex = 1;
+            btnStart.Text = "START";
+            btnStart.UseVisualStyleBackColor = false;
+            // 
+            // lblPlayer1
+            // 
+            lblPlayer1.AutoSize = true;
+            lblPlayer1.Dock = DockStyle.Fill;
+            lblPlayer1.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblPlayer1.Location = new Point(3, 0);
+            lblPlayer1.Name = "lblPlayer1";
+            lblPlayer1.Size = new Size(171, 50);
+            lblPlayer1.TabIndex = 2;
+            lblPlayer1.Text = "Player One";
+            lblPlayer1.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // lblPlayerTwo
+            // 
+            lblPlayerTwo.AutoSize = true;
+            lblPlayerTwo.Dock = DockStyle.Fill;
+            lblPlayerTwo.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblPlayerTwo.Location = new Point(624, 0);
+            lblPlayerTwo.Name = "lblPlayerTwo";
+            lblPlayerTwo.Size = new Size(173, 50);
+            lblPlayerTwo.TabIndex = 3;
+            lblPlayerTwo.Text = "Player Two";
+            lblPlayerTwo.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // tbl1
+            // 
+            tbl1.ColumnCount = 1;
+            tbl1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 100F));
+            tbl1.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 20F));
+            tbl1.Controls.Add(lblScore1, 0, 0);
+            tbl1.Controls.Add(txtScore1, 0, 1);
+            tbl1.Dock = DockStyle.Fill;
+            tbl1.Location = new Point(3, 53);
+            tbl1.Name = "tbl1";
+            tbl1.RowCount = 3;
+            tbl1.RowStyles.Add(new RowStyle(SizeType.Percent, 28.5714283F));
+            tbl1.RowStyles.Add(new RowStyle(SizeType.Percent, 71.42857F));
+            tbl1.RowStyles.Add(new RowStyle(SizeType.Absolute, 20F));
+            tbl1.Size = new Size(171, 312);
+            tbl1.TabIndex = 4;
+            // 
+            // lblScore1
+            // 
+            lblScore1.Anchor = AnchorStyles.Bottom;
+            lblScore1.AutoSize = true;
+            lblScore1.Font = new Font("Segoe UI Semibold", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblScore1.Location = new Point(50, 58);
+            lblScore1.Name = "lblScore1";
+            lblScore1.Size = new Size(70, 25);
+            lblScore1.TabIndex = 0;
+            lblScore1.Text = "SCORE";
+            // 
+            // txtScore1
+            // 
+            txtScore1.Location = new Point(3, 86);
+            txtScore1.Multiline = true;
+            txtScore1.Name = "txtScore1";
+            txtScore1.Size = new Size(165, 89);
+            txtScore1.TabIndex = 1;
+            // 
+            // tableLayoutPanel1
+            // 
+            tableLayoutPanel1.ColumnCount = 1;
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 100F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 20F));
+            tableLayoutPanel1.Controls.Add(txtScore2, 0, 1);
+            tableLayoutPanel1.Controls.Add(lblScore2, 0, 0);
+            tableLayoutPanel1.Dock = DockStyle.Fill;
+            tableLayoutPanel1.Location = new Point(624, 53);
+            tableLayoutPanel1.Name = "tableLayoutPanel1";
+            tableLayoutPanel1.RowCount = 2;
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 28.5714283F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 71.42857F));
+            tableLayoutPanel1.Size = new Size(173, 312);
+            tableLayoutPanel1.TabIndex = 5;
+            // 
+            // txtScore2
+            // 
+            txtScore2.Location = new Point(3, 92);
+            txtScore2.Multiline = true;
+            txtScore2.Name = "txtScore2";
+            txtScore2.Size = new Size(165, 89);
+            txtScore2.TabIndex = 2;
+            // 
+            // lblScore2
+            // 
+            lblScore2.Anchor = AnchorStyles.Bottom;
+            lblScore2.AutoSize = true;
+            lblScore2.Font = new Font("Segoe UI Semibold", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblScore2.Location = new Point(51, 64);
+            lblScore2.Name = "lblScore2";
+            lblScore2.Size = new Size(70, 25);
+            lblScore2.TabIndex = 1;
+            lblScore2.Text = "SCORE";
+            // 
+            // txtMsg
+            // 
+            txtMsg.BackColor = Color.Blue;
+            txtMsg.Dock = DockStyle.Fill;
+            txtMsg.Font = new Font("Goudy Stout", 21.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            txtMsg.ForeColor = Color.Red;
+            txtMsg.Location = new Point(180, 3);
+            txtMsg.Multiline = true;
+            txtMsg.Name = "txtMsg";
+            txtMsg.ReadOnly = true;
+            txtMsg.Size = new Size(438, 44);
+            txtMsg.TabIndex = 6;
+            txtMsg.TextAlign = HorizontalAlignment.Center;
             // 
             // EmojiGame
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(tblMain);
             Name = "EmojiGame";
             Text = "Emoji Memory Game";
+            tblMain.ResumeLayout(false);
+            tblMain.PerformLayout();
+            tblButton.ResumeLayout(false);
+            tbl1.ResumeLayout(false);
+            tbl1.PerformLayout();
+            tableLayoutPanel1.ResumeLayout(false);
+            tableLayoutPanel1.PerformLayout();
             ResumeLayout(false);
         }
 
         #endregion
+
+        private TableLayoutPanel tblMain;
+        private TableLayoutPanel tblButton;
+        private Button btn20;
+        private Button btn19;
+        private Button btn18;
+        private Button btn17;
+        private Button btn16;
+        private Button btn15;
+        private Button btn14;
+        private Button btn13;
+        private Button btn12;
+        private Button btn11;
+        private Button btn10;
+        private Button btn9;
+        private Button btn8;
+        private Button btn7;
+        private Button btn6;
+        private Button btn5;
+        private Button btn4;
+        private Button btn3;
+        private Button btn2;
+        private Button btn1;
+        private Button btnStart;
+        private Label lblPlayer1;
+        private Label lblPlayerTwo;
+        private TableLayoutPanel tbl1;
+        private Label lblScore1;
+        private TableLayoutPanel tableLayoutPanel1;
+        private Label lblScore2;
+        private TextBox txtScore1;
+        private TextBox txtScore2;
+        private TextBox txtMsg;
+        //private Label label1;
     }
 }
--- EmojiMemoryGameApp/EmojiMemoryGameApp/EmojiGame.cs
+++ EmojiMemoryGameApp/EmojiMemoryGameApp/EmojiGame.cs
@@ -1,10 +1,211 @@
+﻿using System.ComponentModel.Design;
+using System.Diagnostics;
+
 namespace EmojiMemoryGameApp
 {
     public partial class EmojiGame : Form
     {
+
+        bool gamestarted = false;
+        List<string> lstemojis;
+        List<Button> lstbuttons;
+
+        enum PlayerUpEnum { playerone, playertwo };
+        PlayerUpEnum playerup = PlayerUpEnum.playerone;
+
+        Button cardone;
+        Button cardtwo;
+
+        int numtimesclicked =0;
+        int nonvisible = 0;
+        string winner;
         public EmojiGame()
         {
             InitializeComponent();
+            btnStart.Click += BtnStart_Click;
+            lstemojis = new() {"😊","😂","😍","😒","😘","😁","😢","😜","😎","😉", "😊", "😂", "😍", "😒", "😘", "😁", "😢", "😜", "😎", "😉" };
+            lstbuttons = new() { btn1, btn2, btn3 , btn4, btn5, btn6, btn7, btn8, btn9, btn10, btn11, btn12, btn13, btn14, btn15, btn16, btn17, btn18, btn19, btn20 };
+            lstbuttons.ForEach(b => b.Click += B_Click);
+            txtMsg.Text = "Press Start";
+        
         }
+
+        //😊😂😍😒😘😁😢😜😎😉
+
+        private void CheckForWinner()
+        {
+            int score1;
+            int score2;
+            bool scoreB1 = int.TryParse(txtScore1.Text, out score1);
+            bool scoreB2 = int.TryParse(txtScore2.Text, out score2);
+            if(score1 > score2 == true)
+            {
+                winner = "The Winner is Player One";
+            }
+            else if (score2 > score1)
+            {
+                winner = "The Winner is Player Two";
+            }
+            else if (score1 == score2)
+            {
+                winner = "It's a Tie";
+            }
+            if(nonvisible == 20)
+            {
+                MessageBox.Show(winner);
+            }
+            else if (txtScore1.Text == "8" || txtScore2.Text == "8")
+            {
+                MessageBox.Show(winner);
+            }
+        }
+        private void SwitchTurn()
+        {
+            if (playerup == PlayerUpEnum.playerone)
+            {
+                playerup = PlayerUpEnum.playertwo;
+            }
+            else
+            {
+                playerup = PlayerUpEnum.playerone;
+            }
+
+            numtimesclicked = 0;
+            cardone = null;
+            cardtwo = null;
+        }
+        private void ChooseCard(Button btn)
+        {
+            btn.Enabled = false;
+            if (numtimesclicked < 3)
+            {
+                Random rnd = new();
+                int n = rnd.Next(lstemojis.Count());
+               if (btn.Text == "") 
+               {
+                    btn.Text = lstemojis[n];
+                    lstemojis.RemoveAt(n);
+                    Font emojifont = new("Arial", 30);
+                    btn.Font = emojifont;
+                    btn.ForeColor = Color.Black;
+               }
+               else if(btn.ForeColor == Color.Yellow)
+               {
+                    btn.ForeColor = Color.Black;
+               }
+
+                switch (numtimesclicked)
+                {
+                    case 1:
+                        cardone = btn;
+                        break;
+                    case 2:
+                        cardtwo = btn;
+                        break;
+                }
+
+                if (cardtwo != null)
+                {
+                    CheckForMatch();
+                    cardone.Enabled = true;
+                    cardtwo.Enabled = true;
+                }
+            }
+        }
+
+        private void CheckForMatch()
+        {
+            if(cardone.Text == cardtwo.Text)
+            {
+                MessageBox.Show("YOU GOT A MATCH!");
+                cardone.Visible = false;
+                cardtwo.Visible = false;
+                nonvisible = nonvisible + 2;
+
+                if (playerup == PlayerUpEnum.playerone)
+                {
+                    int number = 0;
+                    bool prs = int.TryParse(txtScore1.Text, out number);
+                    txtScore1.Text = (number + 1).ToString();
+                }
+                else if(playerup == PlayerUpEnum.playertwo)
+                {
+                    int number2 = 0;
+                    bool prs2 = int.TryParse(txtScore2.Text, out number2);
+                    txtScore2.Text = (number2 + 1).ToString();
+                }
+            }
+            else if(cardone.Text != cardtwo.Text)
+            {
+                MessageBox.Show("NO MATCH!");
+                cardone.ForeColor = Color.Yellow;
+                cardtwo.ForeColor = Color.Yellow;
+                
+            }
+        }
+
+        private void PlayerUp()
+        {
+           
+            if (playerup == PlayerUpEnum.playerone)
+            {
+                txtMsg.Text = "Player One";
+            }
+            else if( playerup == PlayerUpEnum.playertwo)
+            {
+                txtMsg.Text = "Player Two";
+            }
+        }
+
+        private string GetButtonText()
+        {
+            string x = btnStart.Text;
+            return x;
+        }
+        private void BtnStart_Click(object? sender, EventArgs e)
+        {
+            if (btnStart.Text == "START")
+            {
+                PlayerUp();
+                lstbuttons.ForEach(l => l.Text = "");
+                gamestarted = true;
+                btnStart.Text = "End Game";
+                GetButtonText();
+            }
+            else if(btnStart.Text == "End Game")
+            {
+                btnStart.Text = "START";
+                GetButtonText();
+                gamestarted = false;
+                txtScore1.Text = "";
+                txtScore2.Text = "";
+                txtMsg.Text = "Press Start";
+                foreach(Button b in lstbuttons)
+                {
+                    b.Visible = true;
+                    b.Text = "";
+                }
+
+            }
+  
+        }
+
+        private void B_Click(object? sender, EventArgs e)
+        {
+            if (gamestarted == true)
+            {
+                
+                numtimesclicked = numtimesclicked +1;
+                Button btn = (Button)sender;
+                ChooseCard(btn);
+                if (cardtwo != null)
+                {
+                    SwitchTurn();
+                }
+                PlayerUp();
+                CheckForWinner();
+            }
+        }
+
     }
 }
