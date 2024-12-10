01e8e3697a13db9d4d2ca7861446f12c3ddb7233
RassiLevine <rassilevine@gmail.com>
2024-12-10 16:22:56 -0500

    #75

---
+++ EmojiMemoryGameApp/EmojiMemoryGameWeb/Pages/EmojiMemoryGame.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace EmojiMemoryGameWeb.Pages
+{
+    public class EmojiMemoryGameModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
